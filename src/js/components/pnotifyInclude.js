74958a44dbfb5b5c1710950f9283fdb797fee5d7
Ksyusha88 <ksyusha.gorelova@gmail.com>
2021-03-10 15:15:45 +0200

    Error-input

--- src/js/components/pnotifyInclude.js
+++ src/js/components/pnotifyInclude.js
@@ -1,22 +1,26 @@
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/Material.css';
-import 'material-design-icons/iconfont/material-icons.css';
+//import 'material-design-icons/iconfont/material-icons.css';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
+import { notice, error, } from '@pnotify/core/dist/PNotify.js';
 
-import {
-  success,
-  error,
-  defaultModules,
-  Stack,
-} from '@pnotify/core/dist/PNotify.js';
+// import {
+//   success,
+//   error,
+//   defaultModules,
+//   Stack,
+// } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
-defaultModules.set(PNotifyMobile, {});
+// defaultModules.set(PNotifyMobile, {});
 
-const myStack = new Stack({
-  dir1: 'down',
-  dir2: 'left',
-  firstpos1: 25,
-  firstpos2: 30,
-  context: document.body,
-});
+// const myStack = new Stack({
+//   dir1: 'down',
+//   dir2: 'left',
+//   firstpos1: 25,
+//   firstpos2: 30,
+//   context: document.body,
+// });
+
+
+
